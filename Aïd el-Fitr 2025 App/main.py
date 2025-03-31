60cbafcecfa7a8c4b63075f92da7d1a13eb608f2
Sofiane Chehboune <138320534+SofianeChehboune@users.noreply.github.com>
2025-03-31 01:10:25 +0100

    main.py

--- Aïd el-Fitr 2025 App/main.py
+++ Aïd el-Fitr 2025 App/main.py
@@ -109,11 +109,13 @@ with stylable_container(
     """
 ):
     import streamlit as st
+    
     st.markdown("""
     <iframe width="560" height="315" 
-    src="https://www.youtube.com/embed/G-njYClSVgE?autoplay=1&mute=0" 
-    frameborder="0" allowfullscreen></iframe>
+    src="https://www.youtube.com/embed/G-njYClSVgE?autoplay=1" 
+    frameborder="0" allow="autoplay" allowfullscreen></iframe>
 """, unsafe_allow_html=True)
+
 
 
 
